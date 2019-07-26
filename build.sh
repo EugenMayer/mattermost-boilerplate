15c26640eb2aa423775896a64a1912bd6823994d
em <eugen.mayer@kontextwork.de>
2019-07-26 23:17:16 +0200

    polishing gitignore

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ VERSION=${1:-5.13.2}
 IMAGE_BASE_NAME=${2:-eugenmayer/mattermost}
 
 git clone https://github.com/mattermost/mattermost-docker
+
 pushd mattermost-docker
+git pull
 
 pushd app
 docker build --build-arg edition=team --build-arg MM_VERSION=${VERSION} -t ${IMAGE_BASE_NAME}-app:team-${VERSION} .
