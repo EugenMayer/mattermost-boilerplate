a1dc2ca3aa40d75b39275e42cd214c5b7115fe56
em <eugen.mayer@kontextwork.de>
2019-10-12 11:59:43 +0200

    add tagging to releases

--- publish.sh
+++ publish.sh
@@ -5,3 +5,6 @@ IMAGE_BASE_NAME=${2:-eugenmayer/mattermost}
 
 docker push ${IMAGE_BASE_NAME}-app:team-${VERSION}
 docker push ${IMAGE_BASE_NAME}-db:team-${VERSION}
+
+git tag ${VERSION}
+git push --tags
